ee7ceb28e076ca434f1148b1d8b913611614a21b
abdul1ah <almamun01817@gmail.com>
2023-10-07 21:23:29 +0600

    all data show

--- src/components/Pages/AllJewellery/AllJewellery.jsx
+++ src/components/Pages/AllJewellery/AllJewellery.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const AllJewellery = () => {
+    const [ jewellerys, setJewellerys ] = useState([])
+    //     axios.get('/http://localhost:5000/alljewellery')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+
+    axios.get('http://localhost:5000/alljewellery')
+        .then(data => {
+
+            setJewellerys(data.data);
+
+        })
+        .catch(error => {
+            // Handle any errors that occur
+            console.error('Error:', error);
+        });
+
+
+
     return (
         <div>
-            
+            <ul>
+                {
+                    jewellerys.map(jewellery=>
+                        <div key={jewellery._id}>
+                            <div>
+                                <img className='w-96 h-96' src={jewellery.image} alt="" />
+                            </div>
+                            <div>
+                                <h1>{jewellery.name}</h1>
+                                <h1>{jewellery.description}</h1>
+                            </div>
+                        </div>
+                        )
+                }
+            </ul>
         </div>
     );
 };
