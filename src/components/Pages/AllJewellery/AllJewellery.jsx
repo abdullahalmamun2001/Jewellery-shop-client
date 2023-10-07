78108ad65ac650b9b25f32db5d67680a8c0f14a0
abdul1ah <almamun01817@gmail.com>
2023-10-07 22:02:58 +0600

    all jewelary done

--- src/components/Pages/AllJewellery/AllJewellery.jsx
+++ src/components/Pages/AllJewellery/AllJewellery.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const AllJewellery = () => {
-    const [ jewellerys, setJewellerys ] = useState([])
+    const [jewellerys, setJewellerys] = useState([])
     //     axios.get('/http://localhost:5000/alljewellery')
     //   .then(function (response) {
     //     // handle success
@@ -23,21 +24,25 @@ const AllJewellery = () => {
 
     return (
         <div>
-            <ul>
+            <div className='grid grid-cols-2 w-11/12 mx-auto'>
                 {
-                    jewellerys.map(jewellery=>
-                        <div key={jewellery._id}>
-                            <div>
-                                <img className='w-96 h-96' src={jewellery.image} alt="" />
-                            </div>
-                            <div>
-                                <h1>{jewellery.name}</h1>
-                                <h1>{jewellery.description}</h1>
+                    jewellerys.map(jewellery =>
+                        <div key={jewellery._id} >
+                            <div className='flex'>
+                                <div>
+                                    <img className='w-72 h-72' src={jewellery.image} alt="" />
+                                </div>
+                                <div className='m-10'>
+                                    <h1 className='text-3xl'>{jewellery.name}</h1>
+                                    <h1 className='text-xl text-fuchsia-500 mt-4'>{jewellery.description}</h1>
+                                    <p className='text-xl fw-bold mt-6'>Price : {jewellery.price}$</p>
+                                    <button className='btn bg-fuchsia-700 bottom-0 mt-10 '><Icon className='text-xl' icon="fluent:cart-24-filled" />Add to Cart</button>
+                                </div>
                             </div>
                         </div>
-                        )
+                    )
                 }
-            </ul>
+            </div>
         </div>
     );
 };
