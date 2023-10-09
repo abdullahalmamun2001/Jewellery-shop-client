17b2bc9ea67a7e77ae5b3588d4bdea8eee0c6cb0
abdul1ah <almamun01817@gmail.com>
2023-10-09 15:53:32 +0600

    lottie and swal

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -3,6 +3,9 @@ import { AuthContext } from '../../providers/AuthProvider';
 import { Icon } from '@iconify/react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Lottie from "lottie-react";
+import login from './../../../../public/login.json'
+import Swal from 'sweetalert2';
 
 const Login = () => {
     const navigate=useNavigate();
@@ -16,9 +19,29 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 const loggedUser = result.user;
+                if(loggedUser){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Login Successfully',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
                 console.log(loggedUser);
                 // navigate
                 navigate('/')
+            })
+            .catch(error=>{
+                if(error){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'error',
+                        title: "Your Email and Password don't match",
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
             })
 
     }
@@ -26,6 +49,15 @@ const Login = () => {
         googleSignIn()
             .then(result => {
                 const loggedUser=result.user;
+                if(loggedUser){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Login Successfully',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
                 const email=loggedUser.email;
                 const name=loggedUser.displayName;
                 
@@ -40,15 +72,28 @@ const Login = () => {
                    console.log(res.data);
             })
             })
-            .catch(error => { })
+            .catch(error => {
+              if(error){
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'error',
+                    title: "Your Email Password don't match",
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+              }
+            })
     }
     return (
         <div>
             <div className="hero min-h-screen bg-fuchsia-300">
                 <div className="hero-content grid grid-cols-2">
-                    <div className="text-center lg:text-left">
+                    {/* <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div> */}
+                    <div>
+                    <Lottie animationData={login} loop={true} />
                     </div>
                     <div className="card w-[500px] mx-auto shadow-2xl bg-fuchsia-800">
                         <form className="card-body" onSubmit={handleLogin} >
